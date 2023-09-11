55dacee466d6c88fe3fa17c3e42afd8509404a4b
Ashley Grevelink <ashleyg@web3.foundation>
2023-09-11 14:05:13 -0400

    update ui with buttons

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,40 @@ export default function Page() {
         <h1 className="text-xl font-medium text-gray-300">
           welcome to brain-swap!
         </h1>
+
+        <div className="space-y-10 text-white">
+          {demos.map((section) => {
+            return (
+              <div key={section.name} className="space-y-5">
+                <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
+                  {section.name}
+                </div>
+
+                <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+                  {section.items.map((item) => {
+                    return (
+                      <div key={section.name}>
+                        <div className="font-medium text-gray-200 group-hover:text-gray-50">
+                          {item.name}
+                        </div>
+
+                        <div>
+                          {item.description ? (
+                            <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
+                              {item.description}
+                            </div>
+                          ) : null}
+                        </div>
+
+                        <Button>X</Button>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </NextUIProvider>
   );
