ced45c18d4ba59cc79a226efca38cecb6dede1f2
Ashley Grevelink <ashleyg@web3.foundation>
2023-09-11 13:06:58 -0400

    basic app structure (one col of cards)

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,20 +30,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="[color-scheme:dark]">
       <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
-        <GlobalNav />
-
-        <div className="lg:pl-72">
+        <div className="">
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
-            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
-              <div className="rounded-lg bg-black">
-                <AddressBar />
-              </div>
-            </div>
-
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20"></div>
             <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
             </div>
-            <Byline className="fixed sm:hidden" />
           </div>
         </div>
       </body>
